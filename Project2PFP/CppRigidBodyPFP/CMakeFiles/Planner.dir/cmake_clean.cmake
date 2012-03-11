cc11aeed45525f1dc4c88ad62b7928882da7b307
lordoku <lordoku@ubuntu.ubuntu-domain>
2012-03-10 19:36:23 -0500

    Fixed compile issues

---
+++ Project2PFP/CppRigidBodyPFP/CMakeFiles/Planner.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Planner.dir/src/Graphics.o"
+  "CMakeFiles/Planner.dir/src/RigidBodySimulator.o"
+  "CMakeFiles/Planner.dir/src/RigidBodyPlanner.o"
+  "bin/Planner.pdb"
+  "bin/Planner"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Planner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
