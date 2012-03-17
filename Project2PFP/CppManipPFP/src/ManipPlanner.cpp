0e5aec431406045c1f90598d9c78fdeb9f071927
lordoku <coconnell84@gmail.com>
2012-03-17 19:05:14 -0400

    Started working on part2. Arm sometimes moves to goal and if the goal is out of reach, it freaks out.

--- Project2PFP/CppManipPFP/src/ManipPlanner.cpp
+++ Project2PFP/CppManipPFP/src/ManipPlanner.cpp
@@ -1,4 +1,5 @@
 #include "ManipPlanner.hpp"
+#include "Windows.h"
 
 ManipPlanner::ManipPlanner(ManipSimulator * const manipSimulator)
 {
@@ -12,6 +13,23 @@ ManipPlanner::~ManipPlanner(void)
 
 void ManipPlanner::ConfigurationMove(double allLinksDeltaTheta[])
 {
+
+	int numJoints = m_manipSimulator->GetNrLinks()-1;
    
+	double fkX = m_manipSimulator->GetLinkEndX(numJoints) - m_manipSimulator->GetGoalCenterX();
+	double fkY = m_manipSimulator->GetLinkEndY(numJoints)- m_manipSimulator->GetGoalCenterY();
+	
+	double aJaco[2][1];
+	for(int i=0;i<numJoints;i++)
+	{
+		aJaco[0][0] = -m_manipSimulator->GetLinkEndY(numJoints) + -m_manipSimulator->GetLinkStartY(i);
+		aJaco[1][0] = -m_manipSimulator->GetLinkEndX(numJoints) -  m_manipSimulator->GetLinkStartX(i);
+
+		allLinksDeltaTheta[i] = ( (fkX *aJaco[0][0]) + (fkY * aJaco[1][0]) ) / 100;
+	}
+
+	//Now multiply out
+
+	
 }
 
