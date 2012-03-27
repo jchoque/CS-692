0c8fad98b64e8454f664beebd43182611a7d65ad
lordoku <coconnell84@gmail.com>
2012-03-26 21:27:13 -0400

    Added the pseudo code and first run of repulsive refactoring

--- Project2PFP/CppManipPFP/src/ManipPlanner.cpp
+++ Project2PFP/CppManipPFP/src/ManipPlanner.cpp
@@ -60,29 +60,30 @@ void ManipPlanner::ConfigurationMove(double allLinksDeltaTheta[])
 
  void ManipPlanner::CalculateRepulsion(double allLinksDeltaTheta[])
  {
-	 for(int i=0;i<m_manipSimulator->GetNrObstacles();i++)
+	 for(int j=0;j<m_manipSimulator->GetNrLinks(); j++)
 	 {
-		for(int j=0;j<m_manipSimulator->GetNrLinks(); j++)
+		for(int k=0;k<=j; k++)
 		{
-			
 			Jacobian2x2 aJaco(0,0);
-			for(int k=0;k<=j;k++)
-			{
-				aJaco.jacoX += (-m_manipSimulator->GetLinkEndY(j) + m_manipSimulator->GetLinkStartY(k));
-				aJaco.jacoY += (m_manipSimulator->GetLinkEndX(j) -m_manipSimulator->GetLinkStartX(k));
-			}
-
+			aJaco.jacoX += (-m_manipSimulator->GetLinkEndY(j) + m_manipSimulator->GetLinkStartY(k));
+			aJaco.jacoY += (m_manipSimulator->GetLinkEndX(j) -m_manipSimulator->GetLinkStartX(k));
+			
 			double fkX = m_manipSimulator->GetLinkEndX(j);
 			double fkY = m_manipSimulator->GetLinkEndY(j);
 
-			Point closestPoint = m_manipSimulator->ClosestPointOnObstacle(i,fkX,fkY);
+			for(int i=0;i<m_manipSimulator->GetNrObstacles();i++)
+			{
+				Point closestPoint = m_manipSimulator->ClosestPointOnObstacle(i,fkX,fkY);
+				double deltaX = closestPoint.m_x - fkX;
+				double deltaY = closestPoint.m_y - fkY;
+				double distance = sqrt(pow(deltaX,2) + pow(deltaY,2));
+				double uRepX = deltaX/distance;
+				double uRepY = deltaY/distance;
+				
+				double jacoMultiply = 2*(uRepX *aJaco.jacoX) + (uRepY * aJaco.jacoY);
+				allLinksDeltaTheta[j] -=jacoMultiply;
+			}
 
-			double uRepX = closestPoint.m_x - fkX;
-			double uRepY = closestPoint.m_y - fkY;
-
-			double jacoMultiply = 2*(uRepX *aJaco.jacoX) + (uRepY * aJaco.jacoY);
-			
-			allLinksDeltaTheta[j] -=jacoMultiply;
 		}
 	}
  }
