05303c557719d2b91c5a18297f07117e153cf9b1
lordoku <coconnell84@gmail.com>
2012-05-07 22:09:14 -0400

    Trying to get the revision back in order

--- Project2PFP/CppRigidBodyPFP/src/RigidBodyPlanner.cpp
+++ Project2PFP/CppRigidBodyPFP/src/RigidBodyPlanner.cpp
@@ -47,11 +47,50 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 
 		const double *robotVertices = m_simulator->GetRobotVertices();
 		
+<<<<<<< HEAD
 		// Loop over all of the vertices on the robot
 		for(int idx = 0; idx<m_simulator->GetNrRobotVertices(); idx++)
 		{
 			double pointX = robotVertices[2*idx];
 			double pointY = robotVertices[2*idx+1];
+=======
+		/* Calculate the Forward Kinematics for this vertex j
+		*       / xj cos(theta) - yj sin(theta) + x  \
+		* FKj = |                                      |
+		*       \ xj sin(theta) + yj cos(theta) + y  /
+		* Where j represents the index of the current vertice on the robot
+		 */
+		double fk[2][1];
+		// Normalize these x/y values?
+		fk[0][0] = (pointX * cos(configTheta)) - (pointY*sin(configTheta)) + (configX);
+		fk[1][0] = (pointX * sin(configTheta)) + (pointY*cos(configTheta)) + (configY);
+		
+		/* Calculate the attractive force for this vertex j
+		*                / Goalx \
+		* Uattj = FKj - |         |
+		*                \ Goaly /
+		*
+		 */
+		double attractiveForce[2][1];
+		attractiveForce[0][0] = pointX - goalX;
+		attractiveForce[1][0] = pointY - goalY;
+
+		/* Calculate the repulsion force for all obstacles with respect to
+		* this vertex
+		*           / Oix \
+		* Urepij = |       |  - FKj
+		*           \ Oiy /
+		* Where Oiy and Oix are the closest point to this vertex on obstacle i
+		 */
+		double vertexRepulsiveForce[2];
+		memset(vertexRepulsiveForce, 0, sizeof(vertexRepulsiveForce[0]) * 2 * 1);
+		for (int obsIdx = 0; obsIdx < m_simulator->GetNrObstacles(); obsIdx++){
+
+			//The closest point should pass in the pointX, pointY
+			//Point point = m_simulator->ClosestPointOnObstacle(obsIdx, fk[0][0], fk[1][0]);
+			Point point = m_simulator->ClosestPointOnObstacle(obsIdx, pointX, pointY);
+			//REALLY?
+>>>>>>> origin/master
 			// Used for normalizing calculation
 			double goalDistance = sqrt(pow(pointX - goalX, 2) + pow(pointY - goalY, 2));
 
@@ -90,6 +129,7 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 				
 			}
 
+<<<<<<< HEAD
 			/* Calculate the Jacobian
 			*
 			*       / 1 0 -Xj * sin(theta) - Yj * cos(theta) \
@@ -107,10 +147,44 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 			double  attractiveResult[3];
 			memset(attractiveResult, 0, sizeof(attractiveResult[0]) * 1 * 3);
 			for(int i = 0; i < 3; i++)
+=======
+			// Normalize these x/y values?
+			vertexRepulsiveForce[0] += (pointX - point.m_x);
+			vertexRepulsiveForce[1] += (pointY - point.m_y);
+		}
+		
+		/* Calculate the Jacobian
+		*
+		*       / 1 0 -Xj * sin(theta) - Yj * cos(theta) \
+		* Jj = |                                          |
+		*       \ 0 1  Xj * cos(theta) - Yj * sin(theta) /
+		*
+		 */
+		double  jacobian[2][3];
+		jacobian[0][0] = jacobian[1][1] = 1;
+		jacobian[0][1] = jacobian[1][0] = 0;
+		jacobian[0][2] = (-pointX * sin(configTheta)) - (pointY*cos(configTheta));
+		jacobian[1][2] = (pointX * cos(configTheta)) - (pointY *sin(configTheta));
+		
+		// Calculate the final values for this vertex based on prior Uatt, Urep, Jacobian, and FK
+		double  attractiveResult[3];
+		memset(attractiveResult, 0, sizeof(attractiveResult[0]) * 1 * 3);
+		for(int i = 0; i < 3; i++)
+		{
+			attractiveResult[i] = ( (attractiveForce[0][0]*jacobian[0][i]) + (attractiveForce[1][0]*jacobian[1][i]) );
+		}
+		
+
+		//CHRIS: What is this line doing?
+		for(int i=0;i<1;i++)
+		{
+			for(int j=0;j<3;j++)
+>>>>>>> origin/master
 			{
 				attractiveResult[i] = ( (attractiveForce[0]*jacobian[0][i]) + (attractiveForce[1]*jacobian[1][i]) );
 			}
 
+<<<<<<< HEAD
 			for(int i=0;i<1;i++)
 			{
 				for(int j=0;j<3;j++)
@@ -118,6 +192,10 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 					totalAttractiveForce[j] +=attractiveResult[j];
 				}
 			}
+=======
+		//CHRIS: ALso what is this? 
+		for (int i = 0; i < 2; i++){
+>>>>>>> origin/master
 
 			for(int i = 0; i < 2; i++)
 			{
@@ -125,10 +203,19 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 				totalRepulsiveForce[1] += (vertexRepulsiveForce[1]*jacobian[1][i]);
 			}
 
+<<<<<<< HEAD
 			// Store theta for later angle calculation
 			totalTheta += attractiveForce[0] * jacobian[0][2] + attractiveForce[1] * jacobian[1][2];
 			totalTheta += totalRepulsiveForce[0] * jacobian[0][2] + totalRepulsiveForce[1] * jacobian[1][2];
 
+=======
+
+		//BRIAN: NEED TO UPDATE THIS. IT LOOKS LIKE THE THETA VALUES IS BEING ADDED TO THE FORCES
+		for(int i = 0; i < 3; i++)
+		{
+			totalRepulsiveForce[0] += (vertexRepulsiveForce[0]*jacobian[0][i]);
+			totalRepulsiveForce[1] += (vertexRepulsiveForce[1]*jacobian[1][i]);
+>>>>>>> origin/master
 		}
 
 		double thetaScale = PI/64;
@@ -168,6 +255,7 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 		totalRepulsiveForce[1]/=repDistance;
 		}
 
+<<<<<<< HEAD
 		//totalRepulsiveForce[0] = totalRepulsiveForce[1] = 0;
 //		cout<<"CHRIS SAYS: "<<totalRepulsiveForce[0]<<endl;
 		move.m_dx = -(xyScale*totalAttractiveForce[0] - repScale*totalRepulsiveForce[0]);
@@ -176,6 +264,25 @@ RigidBodyMove RigidBodyPlanner::ConfigurationMove(void)
 	else
 	{
 		cout<<"DONE!"<<endl;
+=======
+	/*
+	if (totalRepulsiveForce[0]  + totalRepulsiveForce[1] < 
+		totalAttractiveForce[0] + totalAttractiveForce[1]){
+		repScale = 0.003;
+		xyScale = 0.0009;
+	}
+	*/
+	
+	cout << "Att\t" << setprecision(4) << totalAttractiveForce[0] << "\t" << setprecision(4) << totalAttractiveForce[1] << "\t::\t" 
+	 	 << "Rep\t" << setprecision(4) << totalRepulsiveForce[0]  << "\t" << setprecision(4) << totalRepulsiveForce[1]  << "\t"
+		 << "repScale\t" << repScale << endl;
+
+	move.m_dx = (-xyScale*totalAttractiveForce[0] + repScale*totalRepulsiveForce[0]);
+	move.m_dy = (-xyScale*totalAttractiveForce[1] + repScale*totalRepulsiveForce[1]);
+
+	// Go in slow mode
+	Sleep(100);
+>>>>>>> origin/master
 	}
 	return move;
 }
